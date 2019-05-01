0157040e42c14591760513e121470f95cb4d6ecb
市川功太 <daiya50.ezweb.ne.jp@gmail.com>
2019-05-01 10:04:25 +0900

    add: headerコンポーネント追加

---
+++ components/organisms/Header.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  header.header
+    p header
+</template>
+<script>
+export default {
+  name: 'AppHeader'
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,28 @@
-<template>
-  <div>
-    <nuxt />
-  </div>
+<template lang="pug">
+  .wrap
+    Header
+    .containar
+      nuxt
 </template>
+
+<script>
+import Header from '~/components/organisms/Header';
+
+export default {
+  name: 'Layout',
+  components: {
+    Header
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.wrap {
+  width: 100%;
+  display: block;
+}
+
+.containar {
+  width: 100%;
+}
+</style>
