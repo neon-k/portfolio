d505d19482e1c6f63b728bb3f95ea8a59d697f44
市川功太 <daiya50.ezweb.ne.jp@gmail.com>
2019-05-01 14:04:55 +0900

    add: sns

---
+++ components/molecules/Sns.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+  .sns
+    ul.sns-list
+      li.sns-list
+        Mail
+</template>
+
+<script>
+import Mail from '~/components/atoms/Mail.vue';
+
+export default {
+  name: 'Sns',
+  components: {
+    Mail
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  width: 300px;
+  height: 200px;
+  box-shadow: 1px 2px 3px 1px rgba(0, 0, 0, 0.2);
+  border: 0.5px solid rgb(57, 72, 85);
+  padding: 10px 20px;
+  margin: 10px 10px;
+  text-align: center;
+}
+
+.wrapper {
+  text-decoration: none;
+}
+
+.card_title {
+  @include font-size(12);
+}
+
+.card_date {
+  font-size: 0.7rem;
+  color: rgb(57, 72, 85);
+  text-align: right;
+}
+
+.card_image {
+  max-height: 100px;
+}
+</style>
---
+++ components/organisms/Footer.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  footer.footer
+   p footer
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+};
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  padding: 40px 0 20px;
+  background-color: $secondary;
+}
+</style>
