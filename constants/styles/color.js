8cce68680e029bca7c1eea5627760e92916e4a7b
市川功太 <daiya50.ezweb.ne.jp@gmail.com>
2020-07-02 17:45:13 +0900

    いらないの全て削除

--- constants/styles/color.js
+++
@@ -1,5 +0,0 @@
-export default {
-  PRIMARY: '#fff',
-  SECONDARY: '#000',
-  TERTIARY: '#9b9b9b'
-};
--- store/mutation-types.js
+++
@@ -1,6 +0,0 @@
-export const ACTIVE_MENU = 'ACTIVE_MENU'; // グローバルメニューの開閉
-export const GET_POST = 'GET_POST'; // 記事情報のAPIの時に使う
-export const SUCCESS_POST = 'SUCCESS_POST'; // 記事情報のAPIが成功した場合に使う
-export const ERROR_POST = 'ERROR_POST'; // 記事情報のAPIが失敗した場合に使う
-export const COMPLETE_POST = 'COMPLETE_POST'; // 記事情報のAPIが失敗した場合に使う
-export const SET_SCROLL = 'SET_SCROLL'; // スクロールの値を格納庫する時に使う。
